741433bf4084610776504732f1793f9d0490d028
kmurp62rulz <kmurp62rulz@gmail.com>
2022-02-14 20:06:02 +1000

    Some changes not working yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { motion } from 'framer-motion';
-import { useRef, useEffect, useState } from 'react'; 
+import { useRef, useEffect, useState, Component } from 'react'; 
 import images from './images';
-import coins from './coins';
+import coinsdata from './coins';
 import { Card } from './components/card';
 
 
 function App() {
 
   const [width, setWidth] = useState(0);
+  const [coins, setCoins] = useState(coinsdata);
   const carousel = useRef();
 
   useEffect(() => {
@@ -30,6 +31,20 @@ function App() {
           })}
         </motion.div>
       </motion.div>
+
+      
+      {/* <div className="example-container">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={{setCoins(coins => [...coins, {name:"New coin", color: "green", price:7302.76, change:"6.63%"}])}}
+        />
+      </div> */}
+      {/* <button onClick={
+        setCoins(coins => [...coins, {name:"New coin", color: "green", price:7302.76, change:"6.63%"}])
+        }>
+        Activate Lasers
+      </button> */}
     </div>
   );
 }
