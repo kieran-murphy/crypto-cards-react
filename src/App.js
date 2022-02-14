409c8321ad113195f40eb432f3cc87e6ed079727
kmurp62rulz <kmurp62rulz@gmail.com>
2022-02-14 23:07:47 +1000

    Add card function is somewhat functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { motion } from 'framer-motion';
+import { Select } from '@chakra-ui/react'
 import { useRef, useEffect, useState, Component } from 'react'; 
 import images from './images';
 import coinsdata from './coins';
@@ -13,9 +14,13 @@ function App() {
   const [coins, setCoins] = useState(coinsdata);
   const carousel = useRef();
 
+  
+  const addCard = () => {
+    setCoins([{name:"New coin", color: "green", price:7302.76, change:"6.63%"}, ...coins])
+  };
+
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-
   }, [])
 
 
@@ -32,19 +37,23 @@ function App() {
         </motion.div>
       </motion.div>
 
-      
-      {/* <div className="example-container">
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={{setCoins(coins => [...coins, {name:"New coin", color: "green", price:7302.76, change:"6.63%"}])}}
-        />
-      </div> */}
-      {/* <button onClick={
-        setCoins(coins => [...coins, {name:"New coin", color: "green", price:7302.76, change:"6.63%"}])
-        }>
-        Activate Lasers
-      </button> */}
+      <div className='add'>
+        <select placeholder='Select option'>
+          <option value='option1'>Option 1</option>
+          <option value='option2'>Option 2</option>
+          <option value='option3'>Option 3</option>
+        </select>
+        
+        <div className="example-container">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={addCard}
+          >
+            Add Card
+          </motion.button>
+        </div>
+      </div>
     </div>
   );
 }
